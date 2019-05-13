d0ff34cf5bc7c8530365877bf5c27606e564c237
garyelephant <garygaowork@gmail.com>
2019-05-13 12:22:28 +0800

    update

--- src/main/java/io/github/interestinglab/waterdrop/input/batch/HBase.java
+++ src/main/java/io/github/interestinglab/waterdrop/input/batch/HBase.java
@@ -1,20 +1,33 @@
 package io.github.interestinglab.waterdrop.input.batch;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import com.typesafe.config.Config;
 import io.github.interestinglab.waterdrop.apis.BaseStaticInput;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.execution.datasources.hbase.HBaseTableCatalog;
 import scala.Tuple2;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * TODO:
  *  1. Config should be replaced by waterdrop typesafe config.
  *  2. Tuple2<Object, String> checkConfig() ==> Boolean
+ *  3. waterdrop-apis in mvn repo, exclude spark jar.
  * */
 public class HBase extends BaseStaticInput {
 
     private Config config;
+    private String catalogJsonString;
 
     @Override
     public void setConfig(Config config) {
@@ -28,12 +41,40 @@ public class HBase extends BaseStaticInput {
 
     @Override
     public Tuple2<Object, String> checkConfig() {
-        return null;
+        if (! this.config.hasPath("catalog_file")) {
+            return Tuple2.apply(false, "please specify schema of hbase table by parameter [catalog_file]");
+        }
+
+        // TODO: (1) get schema string from plugin dir[using Waterdrop Common API].
+        this.catalogJsonString = "";
+        try {
+            this.catalogJsonString = FileUtils.readFileToString(new File(config.getString("catalog_file")));
+        } catch (IOException e) {
+            return Tuple2.apply(false, "Cannot get schema of hbase table by parameter [catalog_file], file maybe not exists or cannot be read");
+        }
+
+        if (StringUtils.isBlank(this.catalogJsonString)) {
+            return Tuple2.apply(false, "Cannot get schema of hbase table by parameter [catalog_file], file content is empty");
+        }
+
+        Gson gson = new Gson();
+        try {
+            JsonObject jsonObject = gson.fromJson(this.catalogJsonString, JsonObject.class);
+        } catch (JsonSyntaxException e) {
+            return Tuple2.apply(false, "Cannot get schema of hbase table by parameter [catalog_file], file content is not a valid json string");
+        }
+
+        return Tuple2.apply(true, "");
     }
 
     @Override
     public Dataset<Row> getDataset(SparkSession spark) {
-        // TODO:
-        return spark.emptyDataFrame();
+
+        Map<String, String> options = Collections.singletonMap(
+                HBaseTableCatalog.tableCatalog(),
+                this.catalogJsonString);
+        return spark.read().options(options)
+                .format("org.apache.spark.sql.execution.datasources.hbase")
+                .load();
     }
 }
